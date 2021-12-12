ff9e00133419bb39d9ff2c7ff32431cf9e9a7726
Javier Perez <202010620@itla.edu.do>
2021-12-11 21:15:09 -0400

    Merge Commit
    Login

---
+++ WindowsFormsApp1/FormLogin.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(138)))), ((int)(((byte)(249)))));
+            this.lbLogin.Location = new System.Drawing.Point(158, 76);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(165, 33);
+            this.lbLogin.TabIndex = 0;
+            this.lbLogin.Text = "Bienvenido";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Sitka Display", 15.75F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnLogin.Location = new System.Drawing.Point(183, 304);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(104, 55);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.AccessibleRole = System.Windows.Forms.AccessibleRole.Indicator;
+            this.tbContraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.tbContraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbContraseña.Font = new System.Drawing.Font("Sitka Display", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbContraseña.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(138)))), ((int)(((byte)(249)))));
+            this.tbContraseña.Location = new System.Drawing.Point(120, 203);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.Size = new System.Drawing.Size(187, 27);
+            this.tbContraseña.TabIndex = 3;
+            this.tbContraseña.Text = "Contraseña";
+            this.tbContraseña.Enter += new System.EventHandler(this.tbContraseña_Enter);
+            this.tbContraseña.Leave += new System.EventHandler(this.tbContraseña_Leave);
+            // 
+            // tbUser
+            // 
+            this.tbUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.tbUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbUser.Font = new System.Drawing.Font("Sitka Display", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(138)))), ((int)(((byte)(249)))));
+            this.tbUser.Location = new System.Drawing.Point(121, 152);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(234, 27);
+            this.tbUser.TabIndex = 4;
+            this.tbUser.Text = "Usuario";
+            this.tbUser.Enter += new System.EventHandler(this.tbUser_Enter);
+            this.tbUser.Leave += new System.EventHandler(this.tbUser_Leave);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(457, 437);
+            this.shapeContainer1.TabIndex = 5;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lineShape2.Enabled = false;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape2.X1 = 118;
+            this.lineShape2.X2 = 354;
+            this.lineShape2.Y1 = 235;
+            this.lineShape2.Y2 = 235;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lineShape1.Enabled = false;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.SelectionColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lineShape1.X1 = 120;
+            this.lineShape1.X2 = 356;
+            this.lineShape1.Y1 = 185;
+            this.lineShape1.Y2 = 185;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(342, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(387, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(58, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(457, 437);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.tbContraseña);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lbLogin);
+            this.Controls.Add(this.shapeContainer1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "FormLogin";
+            this.Opacity = 0.9D;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox tbContraseña;
+        private System.Windows.Forms.TextBox tbUser;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/FormLogin.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Negocios;
+using Capa_Datos.Modelos;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormLogin : Form
+    {
+        MantenimientoEntidades _mantenimiento;
+        private FormLogin()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["StringConnectionSQLServer"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            _mantenimiento = new MantenimientoEntidades(connection);
+        }
+
+        public static FormLogin Instancia { get; set; } = new FormLogin();
+
+        private void lbUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormMenu menu = new FormMenu();
+            menu.Show();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbUser_Enter(object sender, EventArgs e)
+        {
+            if (tbUser.Text == "Usuario")
+            {
+                tbUser.Text = "";
+                
+            }
+        }
+
+        private void tbUser_Leave(object sender, EventArgs e)
+        {
+            if (tbUser.Text == "")
+            {
+                tbUser.Text = "Usuario";
+
+            }
+        }
+
+        private void tbContraseña_Enter(object sender, EventArgs e)
+        {
+            if (tbContraseña.Text == "Contraseña")
+            {
+                tbContraseña.Text = "";
+                tbContraseña.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void tbContraseña_Leave(object sender, EventArgs e)
+        {
+            if (tbContraseña.Text == "")
+            {
+                tbContraseña.Text = "Contraseña";
+                tbContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private bool Validar()
+        {
+            bool isEmpty = true;
+            if (tbUser.Text == "")
+            {
+                MessageBox.Show("Debe insertar su usuario", "Advertencia");
+            }
+            else if (tbContraseña.Text == "")
+            {
+                MessageBox.Show("Debe insertar su contraseña", "Advertencia");
+            }
+            else
+            {
+                isEmpty = false;
+            }
+
+            return isEmpty;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (Validar() == false)
+            {
+                if (_mantenimiento.UsuarioExiste(tbUser.Text) == true)
+                {
+                    Entidades entidad = new Entidades();
+                    entidad = _mantenimiento.EnlistarPorUser(tbUser.Text);
+                    if (entidad.PasswordEntidad == tbContraseña.Text)
+                    {
+                        MessageBox.Show("Bienvenido");
+                        this.Hide();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contraseña incorrecta", "Advertencia");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Este usuario no existe", "Advertencia");
+                }
+            }
+        }
+    }
+}
